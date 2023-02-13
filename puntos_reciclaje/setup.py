090b3eca7780a410ede685aec109f4cfe1f1222e
GerardoGrana <gerardograna@gmail.com>
2023-02-12 21:12:10 -0300

    estructura del proyecto

---
+++ puntos_reciclaje/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='puntos_reciclaje',
+    packages=find_packages(),
+    description='We predict new places to locate recicling spots in City of Mendoza, Argentina.',
+    author='Gerardo Graña',
+    version='0.0.1'
+)
