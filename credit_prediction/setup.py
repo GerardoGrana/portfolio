36e0252c7e18564e554f68d592b24fc176be18d2
GerardoGrana <gerardograna@gmail.com>
2023-02-23 16:16:00 -0300

    credit prediction project

---
+++ credit_prediction/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='credit_prediction',
+    packages=find_packages(),
+    description='Una empresa emisora de tarjetas de crédito desea saber cuáles de sus clientes son candidatos probables a abandonar o emigrar a otra tarjeta, término conocido como "churn"- . Para ello se nos solicitó analizar los datos de sus clientes con la finalidad de predecir futuros abandonos.',
+    author='Gerardo Graña',
+    version='0.0.1'
+)
