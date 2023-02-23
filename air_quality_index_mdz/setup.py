819adda2ca0781d5e7362b54396cab571a2a3d7f
GerardoGrana <gerardograna@gmail.com>
2023-02-23 15:15:07 -0300

    more prohects

---
+++ air_quality_index_mdz/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='air_quality_index_mdz',
+    packages=find_packages(),
+    description='In the context of the Climate Management, the local goverment of City of Mendoza, Argentina, created in 2020 the PLAC (Plan de Acción Climática). Recently this local government added to Airvisual.com 4 quality air stations. Thanks to that City of Mendoza is now reporting It's first air data quality to an extensive global network. In this project we explore data via api connections to Airvisual.com in order to monitor and understand the air quality and lead decisions to favor the local climate management.',
+    author='Gerardo Graña',
+    version='0.0.1'
+)
